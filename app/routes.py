caa5a5c5f56c15191099395c71a137f0f536cecf
YRZiTO <YousifZiTO@gmail.com>
2023-08-11 20:50:58 -0400

    Package Updated - Implemented User Account and Profile Picture!

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from app import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
@@ -44,7 +47,8 @@ def register():
         # Hash the password
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
         # Create a new user
-        user = User(username=form.username.data.lower(), email=form.email.data.lower(), password=hashed_password)
+        # user = User(username=form.username.data.lower(), email=form.email.data.lower(), password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user) # Add the user to the database
         db.session.commit() # Commit the changes to the database
         flash("Your account has been created! You are now able to login.", "success")
@@ -59,7 +63,8 @@ def login():
         return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data.lower()).first()
+        # user = User.query.filter_by(email=form.email.data.lower()).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get("next")
@@ -75,9 +80,35 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_images", picture_filename)
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+    return picture_filename
 
 # Create a route for the account
-@app.route("/account")
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        # Redirect to the account page, this is to prevent the form from resubmitting when the page is refreshed
+        # This is called a POST-GET redirect pattern and is a common practice in web development to prevent duplicate submissions of the form
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_images/" + current_user.image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
