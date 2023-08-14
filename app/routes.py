ea94e48cdfc4b7f574edec4282870bb01016da73
YRZiTO <YousifZiTO@gmail.com>
2023-08-14 19:33:18 -0400

    Package Updated - Implemented CRUD!

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,18 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
-from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask import abort, render_template, url_for, flash, redirect, request
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User, Post
 from app import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
-
-# Create a list of dictionaries to represent the posts
-posts = [
-    {
-        "author": "Yousif Zito",
-        "title": "Welcome to my page!",
-        "content": "Veniam magna culpa consequat quis esse qui officia minim tempor ad qui reprehenderit velit. Ut in eu reprehenderit sit qui officia Lorem veniam quis commodo non ipsum minim. Duis officia aliquip culpa consectetur.",
-        "date_posted": "May 4, 2023"
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Welcome to Jane's page!",
-        "content": "Est aliqua ad aliqua aliquip. Eiusmod tempor reprehenderit labore est ea sint sint est excepteur qui laboris. Proident fugiat exercitation nostrud amet Lorem eiusmod mollit irure consectetur id ex aliqua. Id esse dolor reprehenderit quis laborum. Ullamco fugiat pariatur voluptate ad do eiusmod qui esse amet sunt cillum. Nostrud pariatur anim qui qui eu. Est deserunt nostrud Lorem id laboris ad ea deserunt cillum esse aute consectetur veniam.",
-        "date_posted": "May 5, 2023"
-    },
-]
 
 
 # Create a route for the home page
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 
@@ -117,3 +102,52 @@ def account():
         form.email.data = current_user.email
     image_file = url_for("static", filename="profile_images/" + current_user.image_file)
     return render_template("account.html", title="Account", image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post has been created!", "success")
+        return redirect(url_for("home"))
+    return render_template("create_post.html", title="New Post", form=form, legend="New Post")
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("post", post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title="Update Post", form=form, legend="Update Post")
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("home"))
